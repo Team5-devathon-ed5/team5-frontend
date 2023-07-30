29a4f698d5f598dac137df40a40c7596bccb43e6
Ruben Mantilla <-h>
2023-07-29 20:05:23 -0500

    feat!: add module, router and main component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'profile' },
+  {
+    path: 'profile',
+    loadChildren: () =>
+      import('./features/profile/profile.module').then(m => m.ProfileModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

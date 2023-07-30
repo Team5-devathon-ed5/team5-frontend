3d3fef301102263bfb47867d050f8535cb9895bc
Ruben Mantilla <-h>
2023-07-29 21:42:19 -0500

    fix: add profile-routing.module

--- src/app/features/profile/profile.module.ts
+++ src/app/features/profile/profile.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile.component';
+import { RouterModule } from '@angular/router';
+import { ProfileRoutingModule } from './profile-routing.module';
 
 @NgModule({
   declarations: [ProfileComponent],
-  imports: [CommonModule],
-  exports: [ProfileComponent],
+  imports: [CommonModule, ProfileRoutingModule],
+  exports: [RouterModule],
 })
 export class ProfileModule {}

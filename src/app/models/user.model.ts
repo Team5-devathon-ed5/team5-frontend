b87a0c9fb7441b8c86d8f09072b0214eb1a5a56b
Ruben Mantilla <-h>
2023-07-30 12:20:24 -0500

    docs: add comment models User & UserProfile

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,4 +1,16 @@
-// Se puede usar para el register y el login
+/*
+ * Path: src\app\models\user.model.ts
+ * Modelo para el registro y login de usuarios
+ * Se puede usar para el register y el login
+ * @param email: string
+ * @param password: string
+ * @param name: string
+ * @param lastName: string
+ * @param country: string
+ * @param account_active: boolean
+ * @return User
+ * @description
+ */
 export interface User {
   email: string;
   password: string;
@@ -8,7 +20,28 @@ export interface User {
   account_active: boolean;
 }
 
-// para editar el perfil
+/*
+ * Path: src\app\models\user.model.ts
+ * Se puede usar para editar el perfil
+ * @param email: string
+ * @param password: string
+ * @param name: string
+ * @param lastName: string
+ * @param country: string
+ * @param account_active: boolean
+ * @param city?: string
+ * @param address?: string
+ * @param postalCode?: string
+ * @param phone?: {
+ *   code: string;
+ *  number: number;
+ * share: boolean;
+ * };
+ * @param description?: string
+ * @param image?: string
+ * @return User
+ * @description
+ */
 export interface UserProfile extends User {
   city?: string;
   address?: string;

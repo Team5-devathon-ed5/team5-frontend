0a18b93506042a68b9e33bb40ce1bde1236cec57
Iván Fernández de Pinedo <ivan.fdp@gmail.com>
2023-07-31 19:57:02 +0300

    feat: create HeaderComponent

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AngularMaterialModule } from './modules/angular-material/angular-material.module';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent],
   imports: [CommonModule, AngularMaterialModule],
 })
 export class SharedModule {}

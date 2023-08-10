2810283fccc7bcf9608ff124324ad28c80c8f98b
Iván Fernández de Pinedo <ivan.fdp@gmail.com>
2023-08-10 21:48:10 +0300

    feat: app theme

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isDarkMode = false;
+
+  constructor(
+    private element: ElementRef,
+    private renderer: Renderer2
+  ) {}
+
+  get theme() {
+    return this.isDarkMode ? 'dark-theme' : 'light-theme';
+  }
+  changeMode() {
+    this.isDarkMode = !this.isDarkMode;
+    this.renderer.setAttribute(
+      this.element.nativeElement.ownerDocument.documentElement,
+      'theme',
+      this.theme
+    );
+  }
+}

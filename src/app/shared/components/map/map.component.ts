dda5dfd4b24ba20e25191f757728f8c75367a101
fran <franklynssierracontreras@gmail.com>
2023-08-15 20:35:07 -0400

    feat: componentMapPrueba

--- src/app/shared/components/map/map.component.ts
+++ src/app/shared/components/map/map.component.ts
@@ -1,5 +1,6 @@
 import { Component, AfterViewInit } from '@angular/core';
-import { Map, marker, tileLayer } from 'leaflet';
+import { control, Map, marker, tileLayer } from 'leaflet';
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -23,11 +24,19 @@ export class MapComponent implements AfterViewInit {
         'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png',
         {
           maxZoom: 20,
+          errorTileUrl: '',
+          //en el error image se debe colocar o una url de una imagen o importar la imagen y colocar en donde esta
           attribution:
             '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
         }
       ).addTo(map);
-      marker([coords.latitude, coords.longitude], { title: 'user' }).addTo(map);
+      control.scale().addTo(map);
+
+      marker([coords.latitude, coords.longitude], {
+        title: 'user',
+      })
+        .bindPopup('<h2>Usuario es: pedro</h2>')
+        .addTo(map);
     });
     this.watchPosition();
   }

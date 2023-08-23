ff50c558842db056a8036f51edb2844b2c3ccfd7
Iván Fernández de Pinedo <ivan.fdp@gmail.com>
2023-08-23 16:45:18 +0300

    feat: reset password

--- src/app/features/auth/auth-routing.module.ts
+++ src/app/features/auth/auth-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
 const routes: Routes = [
   {
@@ -13,6 +14,10 @@ const routes: Routes = [
       { path: 'login', component: LoginComponent },
       { path: 'sign-up', component: RegisterComponent },
       { path: 'forgot-password', component: ForgotPasswordComponent },
+      {
+        path: 'reset-password/:payload',
+        component: ResetPasswordComponent,
+      },
       { path: '**', redirectTo: 'login' },
     ],
   },
